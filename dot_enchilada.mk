bb7dbb2b7d1edafc732ef368bc93a3a52da09ab7
Crypted Nanomet <sami.mhatre756@gmail.com>
2021-07-18 00:04:58 +0530

    enchilada: Update redfin FP to July

--- dot_enchilada.mk
+++ dot_enchilada.mk
@@ -43,4 +43,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="OnePlus6-user 8.1.0 OPM1.171019.011 273 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
